f2508e2f10a34d86832980136c6ab489e97a88bf
gamerguy887 <gamerguy887@outlook.com>
2023-07-16 20:52:51 -0400

    Added handshake command
    Added the handshake command to the plugin and filled in some comments!

--- src/main/java/com/gamerguy/rpcommands/RPCommands.java
+++ src/main/java/com/gamerguy/rpcommands/RPCommands.java
@@ -1,13 +1,26 @@
 package com.gamerguy.rpcommands;
 
+import com.gamerguy.rpcommands.commands.HandshakeCommand;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+/*
+    This is the main class of the RPCommands 1.20.1 Spigot plugin!
+    Authors:
+    -Gamerguy887
+    -Zero2 owo
+ */
 
 public final class RPCommands extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
 
+        // This is where our commands get registered!
+        this.getCommand("fly").setExecutor(new HandshakeCommand());// Plugin startup logic
+
+        // This is the startup message for the server!
+        Bukkit.getServer().getConsoleSender().sendMessage("RPCommands v1.0 has started successfully!");
     }
 
     @Override
