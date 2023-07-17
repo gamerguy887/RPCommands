b3070b502b0c074b8b49fee78301e5dd22ee21d6
gamerguy887 <gamerguy887@outlook.com>
2023-07-17 15:23:01 -0400

    Update RPCommands.java

--- src/main/java/com/gamerguy/rpcommands/RPCommands.java
+++ src/main/java/com/gamerguy/rpcommands/RPCommands.java
@@ -19,7 +19,7 @@ public final class RPCommands extends JavaPlugin {
         // This is where our commands get registered!
         this.getCommand("handshake").setExecutor(new HandshakeCommand());
         this.getCommand("greet").setExecutor(new GreetingCommand());
-        
+
         Bukkit.getServer().getConsoleSender().sendMessage("RPCommands v1.0 has started successfully!");
         // sPlugin startup logic
     }
